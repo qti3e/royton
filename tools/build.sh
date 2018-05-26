d4b930463f1a14fab13798993c8ee9ac2783fdd9
Parsa Ghadimi <me@qti3e.com>
2018-05-26 20:02:48 +0430

    Add travis

--- tools/build.sh
+++ tools/build.sh
@@ -2,21 +2,8 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path/.."
 
-sudo apt-get install \
-  libxml2-dev \
-  libcurl4-openssl-dev \
-  libjpeg-dev \
-  libpng-dev \
-  libxpm-dev \
-  libmysqlclient-dev \
-  libpq-dev \
-  libicu-dev \
-  libfreetype6-dev \
-  libldap2-dev \
-  libxslt-dev
 cd php-src
-./buildconf
+./buildconf --force
 ./configure
 make
 ar q libphp.a **/*.o **/*.lo
-popd
